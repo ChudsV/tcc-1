d246909bb4897af96b58ca67c9ddbe303c34e07c
ChudsV <jvchudek90@gmail.com>
2023-09-26 18:03:28 -0300

    Update tempo.js

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,16 @@
 async function tempo(request, response){
     const dynamicData = new Date();
-
+    /*
+    Caso se precisar esconder algum dado sensivel:
+    const apiSecret = process.env.NOME_DA_VARIAVEL;
+    */
+    
     const subRequest = await fetch("https://httpbin.org/get");
     const subResponse = await subRequest.json();
     const url = subResponse.url;
+
+    response.setHeader('Cache-Control','s-maxage=10','stale-while-revalidate');
+
     response.json({
         date: dynamicData.toGMTString(),
         url: url
